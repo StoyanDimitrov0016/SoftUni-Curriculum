479c1ba9fbaf15398f441f522dec3ef38ade5feb
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-08-15 13:37:29 +0300

    Finishing refactoring - project use db

--- 4-JS-WEB/1-JS-Back-end/6-workshop-MongoDB-and-Mongoose/cubicle/src/managers/cubeManager.js
+++ 4-JS-WEB/1-JS-Back-end/6-workshop-MongoDB-and-Mongoose/cubicle/src/managers/cubeManager.js
@@ -1,27 +1,22 @@
 const Cube = require('../models/Cube');
-const uniqid = require('uniqid');
 
 
-const fs = require('fs');
-
-const cubes = JSON.parse(fs.readFileSync('./src/db.json'));
-
-
-function getAll(search, from, to) {
-    let result = cubes.slice();
-
+async function getAll(search, from, to) {
+    let result = await Cube.find().lean();
+    //TODO: implement these filters in the query not here
     if (search) {
-        result = cubes.filter(c => c.name.toLowerCase().includes(search.toLowerCase()));
+        result = result.filter(c => c.name.toLowerCase().includes(search.toLowerCase()));
     }
-
+    
     if (from) {
-        result = cubes.filter(c => c.difficultyLevel >= Number(from));
+        result = result.filter(c => c.difficultyLevel >= Number(from));
     }
-
+    
     if (to) {
-        result = cubes.filter(c => c.difficultyLevel <= Number(to));
+        result = result.filter(c => c.difficultyLevel <= Number(to));
     }
-
+    
+    // console.log('>>>>>>',result);
     return result;
 };
 
@@ -32,23 +27,8 @@ function getById(id) {
 async function create(cubeData) {
     const cube = new Cube(cubeData);
     await cube.save();
+    console.log(cube);
     return cube;
-}
-
-async function persist() {
-    return new Promise((resolve, reject) => {
-        console.log(cubes);
-        fs.writeFile(
-            './src/db.json',
-            JSON.stringify(cubes, null, 2),
-            (err) => {
-                if (err == null) {
-                    resolve();
-                } else {
-                    reject(err);
-                }
-            });
-    });
 }
 
 module.exports = { getAll, getById, create };
