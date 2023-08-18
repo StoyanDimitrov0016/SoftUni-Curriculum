942759cf79a661961a01ce890ae0e4e5fd87ffc0
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-08-18 13:14:04 +0300

    Removing remaining console.log

--- 4-JS-WEB/1-JS-Back-end/6-workshop-MongoDB-and-Mongoose/cubicle/src/controllers/cubeController.js
+++ 4-JS-WEB/1-JS-Back-end/6-workshop-MongoDB-and-Mongoose/cubicle/src/controllers/cubeController.js
@@ -4,7 +4,6 @@ const cubeManager = require('../managers/cubeManager');
 const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
-    console.log(cubeManager.getAll())
     res.render('create');
 });
 
