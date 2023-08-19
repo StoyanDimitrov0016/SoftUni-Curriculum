ff024d3c793238fadbf88cf35aacae56f4812b82
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-08-19 20:37:30 +0300

    Add dynamic nav bar and logout functionality

--- 4-JS-WEB/1-JS-Back-end/7-workshop-session-and-authentication/cubicle/src/middlewares/authMiddleware.js
+++ 4-JS-WEB/1-JS-Back-end/7-workshop-session-and-authentication/cubicle/src/middlewares/authMiddleware.js
@@ -8,7 +8,11 @@ exports.auth = async (req, res, next) => {
     if (token) {
         try {
             const user = await jwt.verify(token, SECRET);
+            
             req.user = user;
+            res.locals.user = user;
+            res.locals.isAuthenticated = true;
+
             next();
         } catch (err) {
             res.clearCookie('auth');
