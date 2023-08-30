e66e5fc0eb06c5ee967c275129cd321a6acfd717
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-08-30 21:20:39 +0300

    Create home controller

---
+++ 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/controllers/homeController.js
@@ -0,0 +1,7 @@
+const homeController = require('express').Router();
+
+homeController.get('/', (req, res) => {
+    res.render('home', { title: 'Home Page' });
+});
+
+module.exports = homeController;
