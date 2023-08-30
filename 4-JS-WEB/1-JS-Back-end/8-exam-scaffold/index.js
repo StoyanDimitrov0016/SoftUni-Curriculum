047880412cfd33e10172a096f445c64093bced84
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-08-30 21:22:11 +0300

    Set start function

--- 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/index.js
+++ 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const expressConfig = require('./config/express');
+const databaseConfig = require('./config/database');
+const routerConfig = require('./config/routes');
 
+//TODO change port 
+const port = 3000;
 
 start();
 
 async function start() {
     const app = express();
+
+    expressConfig(app);
+    await databaseConfig(app);
+    routerConfig(app);
+
+    app.listen(port, () => console.log(`Server is listening on port: ${port}`));
 }
