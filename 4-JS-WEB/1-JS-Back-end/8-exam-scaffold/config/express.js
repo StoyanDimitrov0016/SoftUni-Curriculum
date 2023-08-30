b46f637d94f66c5959ac8af63273985c9dfdee3a
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-08-30 21:19:10 +0300

    Add body parser middleware

--- 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/config/express.js
+++ 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/config/express.js
@@ -7,4 +7,5 @@ module.exports = (app) => {
     app.engine('.hbs', hbs.engine);
     app.set('view engine', '.hbs');
     app.use('/static', express.static('static'));
+    app.use(express.urlencoded({ extended: false }));
 };
