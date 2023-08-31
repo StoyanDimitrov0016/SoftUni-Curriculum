4b73e16917ebfea70d4f1c82a0d620a6258a42cb
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-08-31 14:33:30 +0300

    Error parser & error section in register page

--- 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/controllers/authController.js
+++ 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/controllers/authController.js
@@ -1,4 +1,6 @@
+const { Error } = require('mongoose');
 const { register } = require('../services/userService');
+const { parseError } = require('../util/parser');
 
 const authController = require('express').Router();
 
@@ -7,10 +9,35 @@ authController.get('/register', (req, res) => {
     res.render('register', { title: 'Register page' });
 });
 
-authController.post('/register', async(req, res) => {
-    const token = await register(req.body.username, req.body.password);
-    res.cookie('token', token);
-    res.redirect('/auth/register');
+authController.post('/register', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const repass = req.body.repass;
+
+    try {
+
+        if (username === '' || password === '') {
+            throw new Error('All fields must be filled!');
+        }
+
+        if (password !== repass) {
+            throw new Error('Passwords must be the same!');
+        }
+
+        const token = await register(req.body.username, req.body.password);
+        res.cookie('token', token);
+        res.redirect('/auth/register');
+    } catch (error) {
+        const errors = parseError(error);
+        
+        //TODO: add error msg to actual page
+        res.render('register', {
+            title: 'Register Page',
+            errors,
+            body: { username }
+        });
+    }
+
 });
 
 module.exports = authController;
