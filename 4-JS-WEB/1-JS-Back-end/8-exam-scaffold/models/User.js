720b5482eb1ad80ffd946320df6d0106dd235b70
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-08-31 10:34:15 +0300

    Create register and createSession functions

--- 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/models/User.js
+++ 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/models/User.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 
+//TODO change the fields and their types, require and unique
 const userSchema = new Schema({
     username: { type: String, required: true, unique: true },
     hashedPassword: { type: String, required: true },
---
+++ 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/services/userService.js
@@ -0,0 +1,54 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const JWT_SECRET = 'eba13b027f82a861ea3edc29a9e9435a';
+
+async function register(username, password) {
+    const existingUser = User.findOne({ username }).collation({ locale: 'en', strength: 2 });
+
+    if (existingUser) {
+        throw new Error('This username is taken');
+    }
+
+    const hashedPassword = bcrypt.hash(password, 10);
+
+    const user = User.create({
+        username,
+        hashedPassword
+    });
+
+    // TODO: Check if a session is required after registration, or if users should be
+    // redirected to the login page to create a session.
+
+    const token = createSession(user);
+
+    return token;
+
+}
+
+async function login() {
+
+}
+
+function createSession(user) {
+    const payload = {
+        _id: user._id,
+        username: user.username
+    };
+
+    const token = jwt.sign(payload, JWT_SECRET);
+
+    return token;
+}
+
+function verifyToken() {
+
+}
+
+module.exports = {
+    register,
+    login,
+    verifyToken
+};
