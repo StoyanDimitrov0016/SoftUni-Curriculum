497f96baaa69128a97ba45ccf6346453d92209a4
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-09-04 14:49:29 +0300

    Add greeting msg if the user is logged in

--- 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/middlewares/session.js
+++ 4-JS-WEB/1-JS-Back-end/8-exam-scaffold/middlewares/session.js
@@ -5,13 +5,10 @@ module.exports = () => (req, res, next) => {
     const token = req.cookies.token;
 
     if (token) {
-        console.log('>>>', token);
         try {
             const userData = verifyToken(token);
-            console.log('Read successful, user:', userData.username);
             req.user = userData;
         } catch (err) {
-            console.log('Invalid token!');
             res.clearCookie('token');
             res.redirect('/auth/login');
             return;
