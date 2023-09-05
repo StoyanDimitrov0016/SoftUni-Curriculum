89c0e30ea9a2be576abdebc28ecab35ac67d8872
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-09-05 12:41:37 +0300

    Add create hotel functionality

--- 4-JS-WEB/1-JS-Back-end/9-exam-preparation/1-bookingUni/controllers/hotelController.js
+++ 4-JS-WEB/1-JS-Back-end/9-exam-preparation/1-bookingUni/controllers/hotelController.js
@@ -1,3 +1,5 @@
+const { create } = require('../models/Hotel');
+const { parseError } = require('../util/parser');
 const hotelController = require('express').Router();
 
 
@@ -7,6 +9,31 @@ hotelController.get('/:id/details', (req, res) => {
 
 hotelController.get('/create', (req, res) => {
     res.render('booking/create', { title: 'Create Hotel', user: req.user });
+});
+
+hotelController.post('/create', async (req, res) => {
+    const hotel = {
+        name: req.body.name,
+        city: req.body.city,
+        rooms: Number(req.body.rooms),
+        imageUrl: req.body.imageUrl,
+        owner: req.user._id
+    };
+
+    try {
+        if (Object.values(hotel).some(v => !v)) {
+            throw new Error('All fields must be filled in!');
+        }
+
+        const result = await create(hotel);
+        res.redirect('/');
+    } catch (err) {
+        res.render('booking/create', {
+            title: 'Create Hotel',
+            errors: parseError(err),
+            body: hotel
+        });
+    }
 });
 
 hotelController.get('/:id/edit', (req, res) => {
