d9cb8aa0b491ece65224b9b31b146ff6da55a42e
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-05 19:01:52 +0300

    Create App.js

---
+++ 4-JS-WEB/2-Front-end-Framework/4-workshop-components/client/src/App.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+
+import * as userService from "./services/userService";
+
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Search } from "./components/Search";
+import { UserList } from "./components/UserList";
+
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService.getAll()
+      .then(users => { setUsers(users) })
+      .catch(err => {
+        console.log('Error: ' + err);
+      })
+  }, []);
+
+  const onUserCreateSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    const createdUser = await userService.create(data);
+
+    setUsers(state => [...state, createdUser]);
+  }
+
+  const onUserDelete = async (userId) => {
+    await userService.remove(userId);
+
+    setUsers(state => state.filter(x => x !== userId));
+  };
+
+  const onUserUpdateSubmit = async (e, userId) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    const updatedUser = await userService.update(userId, data);
+
+    setUsers(state => state.map(x => x._id === userId ? updatedUser : x));
+  }
+
+  return (
+    <>
+      <Header />
+
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+
+          <UserList
+            users={users}
+            onUserCreateSubmit={onUserCreateSubmit}
+            onUserDelete={onUserDelete}
+            onUserUpdateSubmit={onUserUpdateSubmit}
+          />
+
+        </section>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
