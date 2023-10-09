ada8f13f5fd08261072b3fd13a70b3f5a3349baa
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-09 20:33:32 +0300

    Make controlled form

--- 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/NewSneakerForm/NewSneakerForm.js
+++ 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/NewSneakerForm/NewSneakerForm.js
@@ -1,35 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import * as sneakerService from '../../services/sneakerService';
 
 export const NewSneakerForm = () => {
+    const [formValues, setFormValues] = useState({
+        brand: '',
+        model: '',
+        imageUrl: '',
+        release: '',
+        designer: '',
+        value: ''
+    })
+
+    const onChangeHandler = (e) => {
+        e.preventDefault();
+
+        const { name, value } = e.target;
+
+        setFormValues(state => ({
+            ...state,
+            [name]: name === 'value' ? Number(value) : value,
+        }));
+    }
+
+    const onSubmitHandler = async(e) => {
+        e.preventDefault();
+
+    }
+
     return (
-        <section id="create">
-            <div className="form">
+        <section id="create" >
+            <div className="form" onSubmit={onSubmitHandler}>
                 <h2>Add item</h2>
-                <form className="create-form">
-                    <input type="text" name="brand" id="shoe-brand" placeholder="Brand" />
-                    <input type="text" name="model" id="shoe-model" placeholder="Model" />
+                <form className="create-form" >
+                    <input
+                        type="text"
+                        name="brand"
+                        id="shoe-brand"
+                        placeholder="Brand"
+                        value={formValues.brand}
+                        onChange={onChangeHandler} />
+
+                    <input
+                        type="text"
+                        name="model"
+                        id="shoe-model"
+                        placeholder="Model"
+                        value={formValues.model}
+                        onChange={onChangeHandler} />
+
                     <input
                         type="text"
                         name="imageUrl"
                         id="shoe-img"
                         placeholder="Image url"
+                        value={formValues.imageUrl}
+                        onChange={onChangeHandler}
                     />
+
                     <input
                         type="text"
                         name="release"
                         id="shoe-release"
                         placeholder="Release date"
+                        value={formValues.release}
+                        onChange={onChangeHandler}
                     />
+
                     <input
                         type="text"
                         name="designer"
                         id="shoe-designer"
                         placeholder="Designer"
+                        value={formValues.designer}
+                        onChange={onChangeHandler}
                     />
-                    <input type="text" name="value" id="shoe-value" placeholder="Value" />
+
+                    <input
+                        type="text"
+                        name="value"
+                        id="shoe-value"
+                        placeholder="Value"
+                        value={formValues.value}
+                        onChange={onChangeHandler} />
                     <button type="submit">post</button>
                 </form>
             </div>
-        </section>
+        </section >
     );
 }
