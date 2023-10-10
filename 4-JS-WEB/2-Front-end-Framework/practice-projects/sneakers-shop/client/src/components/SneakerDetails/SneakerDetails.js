9d8f98d6c36c763040b60fbab99438548da5e045
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-10 16:11:33 +0300

    refactor: Improved SneakerDetails component
    Simplified and enhanced the SneakerDetails component by using hooks, dynamic data fetching, and better code organization.

--- 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/SneakerDetails/SneakerDetails.js
+++ 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/SneakerDetails/SneakerDetails.js
@@ -1,38 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import * as sneakerService from '../../services/sneakerService';
+import { Link } from "react-router-dom";
 
 export const SneakerDetails = () => {
+    const { sneakerId } = useParams()
+    const [sneakerDetails, setSneakerDetails] = useState({});
+
+    useEffect(() => {
+        sneakerService.getOne(sneakerId)
+            .then(sneaker => setSneakerDetails(sneaker))
+            .catch(error => console.log('There is some error with fetching particular sneaker', error));
+    }, [sneakerId]);
+
+    const { brand, model, imageUrl, release, designer, value, _id } = sneakerDetails;
+
     return (
         <section id="details">
             <div id="details-wrapper">
                 <p id="details-title">Shoe Details</p>
                 <div id="img-wrapper">
-                    <img src="./images/travis.jpg" alt="example1" />
+                    <img src={imageUrl} alt={`Sneaker: ${brand} - ${model}`} />
                 </div>
                 <div id="info-wrapper">
-                    <p>
-                        Brand: <span id="details-brand">Air Jordan</span>
-                    </p>
-                    <p>
-                        Model: <span id="details-model">1 Retro High TRAVIS SCOTT</span>
-                    </p>
-                    <p>
-                        Release date: <span id="details-release">2019</span>
-                    </p>
-                    <p>
-                        Designer: <span id="details-designer">Travis Scott</span>
-                    </p>
-                    <p>
-                        Value: <span id="details-value">2000</span>
-                    </p>
+                    <p>Brand: <span id="details-brand">{brand}</span></p>
+                    <p>Model: <span id="details-model">{model}</span></p>
+                    <p>Release date: <span id="details-release">{release}</span></p>
+                    <p>Designer: <span id="details-designer">{designer}</span></p>
+                    <p>Value: <span id="details-value">{value}</span></p>
                 </div>
+
                 {/*Edit and Delete are only for creator*/}
                 <div id="action-buttons">
-                    <a href="" id="edit-btn">
-                        Edit
-                    </a>
-                    <a href="" id="delete-btn">
-                        Delete
-                    </a>
+                    <Link className="edit-btn" to={`/edit/${_id}`}>Edit</Link>
+                    <Link className="delete-btn" to={`/sneakers/${_id}`}>Delete</Link>
                 </div>
             </div>
         </section>
