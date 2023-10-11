7220c41dfed93d21c5775847712fc11a80f9050b
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-11 13:58:32 +0300

    style: Implement CSS Modules in Home component

--- 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/Home/Home.js
+++ 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/Home/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from '../../styles/Home.module.css';
 
 export const Home = () => {
     return (
-        <section id="home">
+        <section id={styles.home}>
             <h1>Welcome to Sole Mates</h1>
             <img src="/images/home.jpg" alt="home" />
             <h2>Browse through the shoe collectibles of our users</h2>
