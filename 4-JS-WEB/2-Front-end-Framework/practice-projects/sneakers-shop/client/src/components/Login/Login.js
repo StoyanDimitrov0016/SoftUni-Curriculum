1eaec954308f596ed7c38da299866678889fa973
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-11 13:54:57 +0300

    style: Implement CSS Modules in Login component

--- 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/Login/Login.js
+++ 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/Login/Login.js
@@ -1,21 +1,22 @@
 import React from "react";
+import styles from '../../styles/Form.module.css';
 import { Link } from "react-router-dom";
 
 export const Login = () => {
     return (
-        <section id="login">
-            <div className="form">
+        <section id={styles.login}>
+            <div className={styles.form}>
                 <h2>Login</h2>
-                <form className="login-form">
-                    <input type="text" name="email" id="email" placeholder="email" />
+                <form className={styles['login-form']}>
+                    <input type="text" name="email" id={styles.email} placeholder="email" />
                     <input
                         type="password"
                         name="password"
-                        id="password"
+                        id={styles.password}
                         placeholder="password"
                     />
                     <button type="submit">login</button>
-                    <p className="message">
+                    <p className={styles.message}>
                         <Link to="/register">Not registered? </Link>
                     </p>
                 </form>
