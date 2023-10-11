495fd3349189381215350b47b1d6e2eb4e4fb43a
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-11 13:58:54 +0300

    style: Implement CSS Modules in NavigationBar component

--- 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/NavigationBar.js/NavigationBar.js
+++ 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/NavigationBar.js/NavigationBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import styles from '../../styles/Navigation.module.css';
+
 import { Link } from 'react-router-dom';
 
 export const NavigationBar = () => {
     return (
         <>
-            <Link id='logo' to="/">
-                <img id="logo-img" src="/images/logo.png" alt="Home page link" />
+            <Link id={styles.logo} to="/">
+                <img id={styles['logo-img']} src="/images/logo.png" alt="Home page link" />
             </Link>
             <nav>
                 <div>
@@ -13,12 +15,12 @@ export const NavigationBar = () => {
                     <Link to="/search">Search </Link>
                 </div>
                 {/* Logged-in users */}
-                <div className="user">
+                <div className={styles.user}>
                     <Link to="/create">Add Pair</Link>
                     <Link to="/logout">Logout </Link>
                 </div>
                 {/* Guest users */}
-                <div className="guest">
+                <div className={styles.guest}>
                     <Link to="/login">Login</Link>
                     <Link to="/register">Register </Link>
                 </div>
