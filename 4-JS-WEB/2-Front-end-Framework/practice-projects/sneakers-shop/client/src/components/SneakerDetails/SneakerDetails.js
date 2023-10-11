9695f3f82c50e6ba6baf6045ec34278d709f84b3
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-11 13:59:36 +0300

    style: Implement CSS Modules in SneakerDetails component

--- 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/SneakerDetails/SneakerDetails.js
+++ 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/SneakerDetails/SneakerDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from '../../styles/Details.module.css';
 import * as sneakerService from '../../services/sneakerService';
 import { Link } from "react-router-dom";
 
@@ -16,24 +17,24 @@ export const SneakerDetails = () => {
     const { brand, model, imageUrl, release, designer, value, _id } = sneakerDetails;
 
     return (
-        <section id="details">
-            <div id="details-wrapper">
-                <p id="details-title">Shoe Details</p>
-                <div id="img-wrapper">
+        <section id={styles.details}>
+            <div id={styles["details-wrapper"]}>
+                <p id={styles["details-title"]}>Shoe Details</p>
+                <div id={styles["img-wrapper"]}>
                     <img src={imageUrl} alt={`Sneaker: ${brand} - ${model}`} />
                 </div>
-                <div id="info-wrapper">
-                    <p>Brand: <span id="details-brand">{brand}</span></p>
-                    <p>Model: <span id="details-model">{model}</span></p>
-                    <p>Release date: <span id="details-release">{release}</span></p>
-                    <p>Designer: <span id="details-designer">{designer}</span></p>
-                    <p>Value: <span id="details-value">{value}</span></p>
+                <div id={styles["info-wrapper"]}>
+                    <p>Brand: <span id={styles["details-brand"]}>{brand}</span></p>
+                    <p>Model: <span id={styles["details-model"]}>{model}</span></p>
+                    <p>Release date: <span id={styles["details-release"]}>{release}</span></p>
+                    <p>Designer: <span id={styles["details-designer"]}>{designer}</span></p>
+                    <p>Value: <span id={styles["details-value"]}>{value}</span></p>
                 </div>
 
                 {/*Edit and Delete are only for creator*/}
-                <div id="action-buttons">
-                    <Link className="edit-btn" to={`/edit/${_id}`}>Edit</Link>
-                    <Link className="delete-btn" to={`/sneakers/${_id}`}>Delete</Link>
+                <div id={styles["action-buttons"]}>
+                    <Link className={styles["edit-btn"]} to={`/edit/${_id}`}>Edit</Link>
+                    <Link className={styles["delete-btn"]} to={`/sneakers/${_id}`}>Delete</Link>
                 </div>
             </div>
         </section>
