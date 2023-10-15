4d36ed998c2adf26420b9cc8dc1e82e51beab962
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-15 22:14:54 +0300

    Make the form in Register component controlled

--- 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/Register/Register.js
+++ 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/Register/Register.js
@@ -1,30 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../../styles/Form.module.css';
-import { Link } from "react-router-dom";
+import { register } from "../../services/userService";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Register = () => {
+    const navigate = useNavigate();
+
+    
+
+    const [formValues, setFormValues] = useState({
+        email: '',
+        password: '',
+        repass: ''
+    });
+
+    const onChangeHandler = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setFormValues(state => ({ ...state, [name]: value }));
+    };
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        const { email, _id, accessToken } = await register(formValues);
+        console.log(email, _id, accessToken);
+        navigate('/catalog');
+    };
+
     return (
         <section id={styles.register}>
             <div className={styles.form}>
                 <h2>Register</h2>
-                <form className={styles.login}>
+                <form className={styles.login} onSubmit={onSubmitHandler}>
                     <input
                         type="text"
                         name="email"
                         id={styles['register-email']}
                         placeholder="email"
+                        value={formValues.email}
+                        onChange={onChangeHandler}
                     />
                     <input
                         type="password"
                         name="password"
                         id={styles['register-password']}
                         placeholder="password"
+                        value={formValues.password}
+                        onChange={onChangeHandler}
                     />
                     <input
                         type="password"
-                        name="re-password"
+                        name="repass"
                         id={styles['repeat-password']}
                         placeholder="repeat password"
+                        value={formValues.repass}
+                        onChange={onChangeHandler}
                     />
                     <button type="submit">login</button>
                     <p className={styles.message}>
