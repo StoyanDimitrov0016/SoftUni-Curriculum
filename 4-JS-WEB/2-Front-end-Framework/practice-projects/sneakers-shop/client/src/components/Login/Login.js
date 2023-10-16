94d2e7224c5509b73a5da65ac2a05d68722ba932
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-16 22:30:09 +0300

    Refactors the Login component to use controlled form inputs and user login functionality

--- 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/Login/Login.js
+++ 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/components/Login/Login.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../../styles/Form.module.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../../services/userService";
+import { useAuth } from "../../context/AuthContext";
 
 export const Login = () => {
+    const navigate = useNavigate();
+
+    const [formValues, setFormValues] = useState({ email: '', password: '' });
+    const { setUserCredentials } = useAuth();
+
+    const onChangeHandler = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setFormValues(state => ({ ...state, [name]: value }));
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        const { email, _id, accessToken } = await login(formValues);
+        setUserCredentials(state => ({ ...state, email, id: _id, accessToken }));
+        
+        navigate('/catalog');
+    }
+
     return (
         <section id={styles.login}>
             <div className={styles.form}>
                 <h2>Login</h2>
-                <form className={styles['login-form']}>
-                    <input type="text" name="email" id={styles.email} placeholder="email" />
+                <form className={styles['login-form']} onSubmit={onSubmitHandler}>
+                    <input
+                        type="text"
+                        name="email"
+                        id={styles.email}
+                        placeholder="email"
+                        value={formValues.email}
+                        onChange={onChangeHandler}
+                    />
+
                     <input
                         type="password"
                         name="password"
                         id={styles.password}
                         placeholder="password"
+                        value={formValues.password}
+                        onChange={onChangeHandler}
                     />
                     <button type="submit">login</button>
                     <p className={styles.message}>
