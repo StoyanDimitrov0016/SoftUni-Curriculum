cb24541d6f567f1fdee1b1e4e905a76b51bfb84e
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-16 22:24:33 +0300

    Refactoring sneakerService to use requestService for requests to the server

--- 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/services/sneakerService.js
+++ 4-JS-WEB/2-Front-end-Framework/practice-projects/sneakers-shop/client/src/services/sneakerService.js
@@ -1,39 +1,18 @@
-const BASE_URL = 'http://localhost:3030/jsonstore';
+import { request } from "./requestService";
+
+const BASE_URL = 'http://localhost:3030';
 
 export const getAll = async () => {
-    const response = await fetch(`${BASE_URL}/sneakers`);
-    const data = await response.json();
-    return data;
+    const res = await request.get(`${BASE_URL}/data/shoes`);
+    console.log(res);
+    return res
 }
 
-export const getOne = async (sneakerId) => {
-    const response = await fetch(`${BASE_URL}/sneakers/${sneakerId}`);
-    const data = await response.json();
-    return data;
-}
+export const getOne = async (sneakerId) => await request.get(`${BASE_URL}/sneakers/${sneakerId}`);
 
-export const create = async (sneakerData) => {
-    const response = await fetch(`${BASE_URL}/sneakers`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(sneakerData),
-    });
+export const create = async (sneakerData, accessToken) => await request.post(`${BASE_URL}/data/shoes`, sneakerData, accessToken);
 
-    if (!response.ok) {
-        throw new Error('Failed to create sneaker'); 
-    }
-
-    const data = await response.json();
-
-    return data;
-}
-
-
-export const edit = async () => {
-
-}
+export const edit = async () => { }
 
 export const remove = async () => {
 
