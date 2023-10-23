c7d52ea2d3cdb7a641dab064991f72118b8d8c27
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-23 21:14:10 +0300

    Add useEffect to fetch the games and pass them to the Catalog component

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/App.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/App.js
@@ -1,14 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import * as gameService from './services/gameService';
 
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
-import { Home } from "./components/Home/Home";
-import { Login } from "./components/Login/Login";
-import { Register } from "./components/Register/Register";
-import { CreateGame } from "./components/CreateGame/CreateGame";
-import { Catalog } from "./components/Catalog/Catalog";
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Home } from './components/Home/Home';
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+import { CreateGame } from './components/CreateGame/CreateGame';
+import { Catalog } from './components/Catalog/Catalog';
 
 function App() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => setGames(result))
+            .catch(error => console.log(error));
+    }, []);
+
     return (
         <div id="box">
             <Header />
@@ -19,7 +29,7 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/create-game" element={<CreateGame />} />
-                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/catalog" element={<Catalog games={games} />} />
                 </Routes>
             </main>
 
