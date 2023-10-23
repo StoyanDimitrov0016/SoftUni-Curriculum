d8742ca5e5dbd982db87bdfaec67cb7c9aa25019
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-23 21:18:46 +0300

    Add CatalogItem component which represents each game

---
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/components/Catalog/CatalogItem/CatalogItem.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export const CatalogItem = ({
+    title,
+    category,
+    maxLevel,
+    imageUrl,
+    summary,
+    _id
+}) => {
+    return (
+        <div className="allGames">
+            <div className="allGames-info">
+                <img src={imageUrl} />
+                <h6>{category}</h6>
+                <h2>{title}</h2>
+                <Link to={`/details/${_id}`} className="details-button">
+                    Details
+                </Link>
+            </div>
+        </div>
+    );
+};
