82e06c7e3ca3f0d613acc3c7eea7b44808f776e9
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-23 19:33:22 +0300

    Add routes to every component

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/App.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { CreateGame } from "./components/CreateGame/CreateGame";
+import { Catalog } from "./components/Catalog/Catalog";
 
 function App() {
     return (
@@ -9,12 +14,18 @@ function App() {
             <Header />
 
             <main id="main-content">
-                <Home />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/create-game" element={<CreateGame />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                </Routes>
             </main>
 
             <Footer />
         </div>
     );
-}
+};
 
 export default App;
