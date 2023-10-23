9d9428663d91eb727b9014ac2aa21da6118d7bf7
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-23 19:30:19 +0300

    Add Header, Footer and Home in App.js

---
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/App.js
@@ -0,0 +1,20 @@
+
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
+
+function App() {
+    return (
+        <div id="box">
+            <Header />
+
+            <main id="main-content">
+                <Home />
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
