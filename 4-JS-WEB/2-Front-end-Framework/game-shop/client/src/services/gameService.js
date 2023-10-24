e4e0824966d1eab557c9e5fa924d53c03044c76e
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-24 14:29:55 +0300

    Add create function in gameService

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/services/gameService.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/services/gameService.js
@@ -4,3 +4,8 @@ export const getAll = async () => {
     const games = await requester.get('/data/games?sortBy=_createdOn%20desc');
     return games;
 };
+
+export const create = async (gameData) => {
+    const result = await requester.post('/data/games', gameData);
+    return result;
+};
