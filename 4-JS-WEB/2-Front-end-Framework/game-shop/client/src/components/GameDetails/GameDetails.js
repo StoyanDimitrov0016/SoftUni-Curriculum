bedfbe5fddb953aa2585646ccfd7ac39acd64bcd
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-24 16:49:27 +0300

    Refactor GameDetails component: 1. Make controlled form 2. Add dynamic rendering to comments

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/components/GameDetails/GameDetails.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/components/GameDetails/GameDetails.js
@@ -1,55 +1,83 @@
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import * as gameService from '../../services/gameService';
+import * as commentService from '../../services/commentService';
+
 export const GameDetails = () => {
+    const { gameId } = useParams();
+    
+    const [game, setGame] = useState({});
+    const [existingComments, setExistingComments] = useState([]);
+    const [comment, setComment] = useState('');
+
+    useEffect(() => {
+        gameService.getOne(gameId)
+            .then(gameData => {
+                setGame(gameData);
+                return commentService.getAll(gameId);
+            })
+            .then(commentsData => setExistingComments(commentsData))
+            .catch(error => console.log(error));
+    }, [gameId]);
+
+    const onCommentSubmit = async (e) => {
+        e.preventDefault();
+
+        const newComment = await commentService.create(gameId, comment);
+        setExistingComments(state => [...state, newComment]);
+        setComment('');
+    };
+
+    const onCommentChange = (e) => {
+        e.preventDefault();
+
+        setComment(e.target.value);
+    };
+
     return (
         <section id="game-details">
             <h1>Game Details</h1>
             <div className="info-section">
                 <div className="game-header">
-                    <img className="game-img" src="images/MineCraft.png" />
-                    <h1>Bright</h1>
-                    <span className="levels">MaxLevel: 4</span>
-                    <p className="type">Action, Crime, Fantasy</p>
+                    <img className="game-img" src={game.imageUrl} alt={game.title} />
+                    <h1>{game.title}</h1>
+                    <span className="levels">MaxLevel: {game.maxLevel}</span>
+                    <p className="type">{game.category}</p>
                 </div>
-                <p className="text">
-                    Set in a world where fantasy creatures live side by side with humans. A
-                    human cop is forced to work with an Orc to find a weapon everyone is
-                    prepared to kill for. Set in a world where fantasy creatures live side
-                    by side with humans. A human cop is forced to work with an Orc to find a
-                    weapon everyone is prepared to kill for.
-                </p>
-                {/* Bonus ( for Guests and Users ) */}
+                <p className="text">{game.summary}</p>
+
                 <div className="details-comments">
                     <h2>Comments:</h2>
                     <ul>
-                        {/* list all comments for current game (If any) */}
-                        <li className="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li className="comment">
-                            <p>Content: The best game.</p>
-                        </li>
+                        {existingComments.map(c => (
+                            <li key={c._id} className="comment">
+                                <p>Content: {c.comment}</p>
+                            </li>)
+                        )}
+
+                        {existingComments.length === 0 && (
+                            <p className="no-comment">No comments.</p>
+                        )}
                     </ul>
-                    {/* Display paragraph: If there are no games in the database */}
-                    <p className="no-comment">No comments.</p>
                 </div>
+
                 {/* Edit/Delete buttons ( Only for creator of this game )  */}
                 <div className="buttons">
-                    <a href="#" className="button">
-                        Edit
-                    </a>
-                    <a href="#" className="button">
-                        Delete
-                    </a>
+                    <Link to={'/'} className="button">Edit</Link>
+                    <Link to={'/'} className="button">Delete</Link>
                 </div>
             </div>
             {/* Bonus */}
             {/* Add Comment ( Only for logged-in users, which is not creators of the current game ) */}
             <article className="create-comment">
                 <label>Add new comment:</label>
-                <form className="form">
+                <form className="form" onSubmit={onCommentSubmit}>
                     <textarea
                         name="comment"
-                        placeholder="Comment......"
-                        defaultValue={""}
+                        placeholder="Comment &hellip;"
+                        value={comment}
+                        onChange={onCommentChange}
                     />
                     <input
                         className="btn submit"
