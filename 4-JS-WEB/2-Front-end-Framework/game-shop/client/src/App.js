e0a86b4571f382af5842a149330c0b73b669c32f
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-24 14:32:48 +0300

    Add onCreateGameSubmit handler for CreateGame and pass it to it

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/App.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import * as gameService from './services/gameService';
 
@@ -10,7 +10,9 @@ import { Register } from './components/Register/Register';
 import { CreateGame } from './components/CreateGame/CreateGame';
 import { Catalog } from './components/Catalog/Catalog';
 
+
 function App() {
+    const navigate = useNavigate();
     const [games, setGames] = useState([]);
 
     useEffect(() => {
@@ -18,6 +20,12 @@ function App() {
             .then(result => setGames(result))
             .catch(error => console.log(error));
     }, []);
+
+    const onCreateGameSubmit = async (data) => {
+        const newGame = await gameService.create(data);
+        setGames(state => [...state, newGame]);
+        navigate('/catalog');
+    };
 
     return (
         <div id="box">
@@ -28,8 +36,9 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
-                    <Route path="/create-game" element={<CreateGame />} />
+                    <Route path="/create-game" element={<CreateGame onCreateGameSubmit={onCreateGameSubmit} />} />
                     <Route path="/catalog" element={<Catalog games={games} />} />
+
                 </Routes>
             </main>
 
