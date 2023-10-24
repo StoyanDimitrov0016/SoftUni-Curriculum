6ef76433ed4ef376083452d356fc4f0303cf9697
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-24 18:10:53 +0300

    Make an array of the first three games and conditional rendering depending if there are games or not

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/components/Home/Home.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/components/Home/Home.js
@@ -1,4 +1,8 @@
-export const Home = () => {
+import { HomeItem } from "./HomeItem/HomeItem";
+
+export const Home = ({ games }) => {
+    const firstThreeGames = games.slice(0, 3);
+
     return (
         <section id="welcome-world">
             <div className="welcome-message">
@@ -8,63 +12,15 @@ export const Home = () => {
             <img src="./images/four_slider_img01.png" alt="hero" />
             <div id="home-page">
                 <h1>Latest Games</h1>
-                {/* Display div: with information about every game (if any) */}
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/CoverFire.png" />
-                    </div>
-                    <h3>Cover Fire</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/ZombieLang.png" />
-                    </div>
-                    <h3>Zombie Lang</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/MineCraft.png" />
-                    </div>
-                    <h3>MineCraft</h3>
-                    <div className="rating">
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                {/* Display paragraph: If there is no games  */}
-                <p className="no-articles">No games yet</p>
+
+                {firstThreeGames.map(g => (
+                    <HomeItem key={g._id} game={g} />
+                ))}
+
+                {firstThreeGames.length === 0 && (
+                    <p className="no-articles">No games yet</p>)
+                }
+
             </div>
         </section>);
 };
