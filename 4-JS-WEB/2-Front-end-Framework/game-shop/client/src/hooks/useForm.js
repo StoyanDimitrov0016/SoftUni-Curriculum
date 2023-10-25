8341cd95836c63a775c5f55ea9a7fd025227e600
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-25 15:41:52 +0300

    Create custom hook useForm

---
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/hooks/useForm.js
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export const useForm = (initialValues, onSubmitHandler) => {
+    const [formValues, setFormValues] = useState(initialValues);
+
+    const changeHandler = (e) => {
+        setFormValues(state => ({ ...state, [e.target.name]: e.target.value }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        onSubmitHandler(formValues);
+    };
+
+    return { formValues, changeHandler, onSubmit };
+}
