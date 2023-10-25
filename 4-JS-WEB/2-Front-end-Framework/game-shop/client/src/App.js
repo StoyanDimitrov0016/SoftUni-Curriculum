b4dfac66be634526ac9e11cb1d8cdc13cf546c96
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-25 17:25:41 +0300

    Make onLoginSubmit to make a request to the server and sets the returning values in auth state. Extracted AuthContext context object into separate variable.

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/App.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/App.js
@@ -1,6 +1,10 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+
 import * as gameService from './services/gameService';
+import * as authService from './services/authService';
+
+import { AuthContext } from './contexts/AuthContext';
 
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
@@ -14,6 +18,7 @@ import { GameDetails } from './components/GameDetails/GameDetails';
 function App() {
     const navigate = useNavigate();
     const [games, setGames] = useState([]);
+    const [auth, setAuth] = useState({});
 
     useEffect(() => {
         gameService.getAll()
@@ -27,23 +32,43 @@ function App() {
         navigate('/catalog');
     };
 
+    const onLoginSubmit = async (data) => {
+        try {
+            console.log(data.email, data.password);
+            const result = await authService.login(data.email, data.password);
+            setAuth(result);
+            navigate('/');
+        } catch (error) {
+            console.log('Error while logging in occurred:', error);
+        }
+    };
+
+    const context = {
+        onLoginSubmit,
+        userId: auth._id,
+        token: auth.accessToken,
+        userEmail: auth.email
+    };
+
     return (
-        <div id="box">
-            <Header />
+        <AuthContext.Provider value={context} >
+            <div id="box">
+                <Header />
 
-            <main id="main-content">
-                <Routes>
-                    <Route path="/" element={<Home games={games} />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/create-game" element={<CreateGame onCreateGameSubmit={onCreateGameSubmit} />} />
-                    <Route path="/catalog" element={<Catalog games={games} />} />
-                    <Route path="/catalog/:gameId" element={<GameDetails />} />
-                </Routes>
-            </main>
+                <main id="main-content">
+                    <Routes>
+                        <Route path="/" element={<Home games={games} />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/create-game" element={<CreateGame onCreateGameSubmit={onCreateGameSubmit} />} />
+                        <Route path="/catalog" element={<Catalog games={games} />} />
+                        <Route path="/catalog/:gameId" element={<GameDetails />} />
+                    </Routes>
+                </main>
 
-            <Footer />
-        </div>
+                <Footer />
+            </div>
+        </AuthContext.Provider>
     );
 };
 
