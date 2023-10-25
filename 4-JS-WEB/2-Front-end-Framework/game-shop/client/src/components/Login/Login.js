3d0ef2bf4b936910bf3b93a691321d2dfae88d4e
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-25 15:42:32 +0300

    Refactor the form in Login component to be controlled

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/components/Login/Login.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/components/Login/Login.js
@@ -1,19 +1,46 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useForm } from "../../hooks/useForm";
+
+
 export const Login = () => {
+    const LoginFormKeys = {
+        Email: 'email',
+        Password: 'password'
+    };
+
+    const { onLoginSubmit } = useContext(AuthContext);
+
+    const { formValues, changeHandler, onSubmit } = useForm({
+        [LoginFormKeys.Email]: '',
+        [LoginFormKeys.Password]: ''
+    }, onLoginSubmit);
+
     return (
         <section id="login-page" className="auth">
-            <form id="login">
+            <form id="login" onSubmit={onLoginSubmit}>
                 <div className="container">
                     <div className="brand-logo" />
                     <h1>Login</h1>
+
                     <label htmlFor="email">Email:</label>
                     <input
                         type="email"
                         id="email"
-                        name="email"
+                        name={LoginFormKeys.Email}
                         placeholder="Sokka@gmail.com"
+                        value={formValues[LoginFormKeys.Email]}
+                        onChange={changeHandler}
                     />
                     <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
+                    <input
+                        type="password"
+                        id="login-password"
+                        name={LoginFormKeys.Password}
+                        value={formValues[LoginFormKeys.Password]}
+                        onChange={changeHandler}
+                    />
+
                     <input type="submit" className="btn submit" defaultValue="Login" />
                     <p className="field">
                         <span>
