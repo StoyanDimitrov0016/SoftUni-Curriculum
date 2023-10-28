03227760481d3bd221cb1af6939120e825aff356
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-28 21:08:38 +0300

    refactor: Introduce commentServiceFactory for authorized requests
    'commentServiceFactory' enables the creation of comment service methods with an authorization token
    for making authorized requests.

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/services/commentService.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/services/commentService.js
@@ -1,11 +1,11 @@
-import * as requester from './requester';
+import { requestFactory } from "./requester";
 
-export const getAll = async (gameId) => {
-    const comments = await requester.get(`/data/comments?where=gameId%3D%22${gameId}%22`);
-    return comments;
+export const commentServiceFactory = (token) => {
+    const request = requestFactory(token);
+
+    return {
+        getAll: async (gameId) => await request.get(`/data/comments?where=gameId%3D%22${gameId}%22`),
+        create: async (gameId, comment) => await request.post('/data/comments', { gameId, comment })
+    };
 };
 
-export const create = async (gameId, comment) => {
-    const result = await requester.post('/data/comments', { gameId, comment });
-    return result;
-};
