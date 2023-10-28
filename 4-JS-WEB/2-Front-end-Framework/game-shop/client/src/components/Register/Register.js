2de24b8ea826be85f2cda6fdd3e3a6dd484ce9c6
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-28 21:14:32 +0300

    refactor: Enhance Register component with form management
    'Register' includes improved form management. It utilizes the 'useForm' hook,
    handles form state, and integrates form submission.

--- 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/components/Register/Register.js
+++ 4-JS-WEB/2-Front-end-Framework/game-shop/client/src/components/Register/Register.js
@@ -5,28 +5,58 @@ import { useForm } from "../../hooks/useForm";
 import { AuthContext } from "../../contexts/AuthContext";
 
 export const Register = () => {
+    const RegisterFormKeys = {
+        Email: 'email',
+        Password: 'password',
+        ConfirmPassword: 'confirm-password'
+    };
 
+    const { onRegisterSubmit } = useContext(AuthContext);
+
+    const { formValues, changeHandler, onSubmit } = useForm({
+        [RegisterFormKeys.Email]: '',
+        [RegisterFormKeys.Password]: '',
+        [RegisterFormKeys.ConfirmPassword]: ''
+    }, onRegisterSubmit);
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" onSubmit={onSubmit}>
                 <div className="container">
                     <div className="brand-logo" />
                     <h1>Register</h1>
+
                     <label htmlFor="email">Email:</label>
                     <input
                         type="email"
                         id="email"
-                        name="email"
+                        name={RegisterFormKeys.Email}
                         placeholder="maria@email.com"
+                        value={formValues[RegisterFormKeys.Email]}
+                        onChange={changeHandler}
                     />
+
                     <label htmlFor="pass">Password:</label>
-                    <input type="password" name="password" id="register-password" />
+                    <input
+                        type="password"
+                        name={RegisterFormKeys.Password}
+                        id="register-password"
+                        value={formValues[RegisterFormKeys.Password]}
+                        onChange={changeHandler}
+                    />
+
                     <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password" />
+                    <input
+                        type="password"
+                        name={RegisterFormKeys.ConfirmPassword}
+                        id="confirm-password"
+                        value={formValues[RegisterFormKeys.ConfirmPassword]}
+                        onChange={changeHandler}
+                    />
+                    
                     <input className="btn submit" type="submit" defaultValue="Register" />
                     <p className="field">
                         <span>
-                            If you already have profile click<Link to='/login'>here</Link>
+                            If you already have profile click <Link to='/login'>here</Link>
                         </span>
                     </p>
                 </div>
