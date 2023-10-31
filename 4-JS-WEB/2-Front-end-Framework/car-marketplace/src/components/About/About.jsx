09c9af5b767fdab9e13c2a2d3d874d69f97864e0
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-31 10:53:51 +0200

    Create About component - its content will be about the history of the company

---
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/src/components/About/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about-section">
+      <h2>Our Story</h2>
+      <p>
+        In the year 1997, a group of passionate automotive enthusiasts came
+        together to realize a shared dream. They aimed to create a platform
+        where people could find their dream vehicles and sellers could connect
+        with genuine buyers. Thus, the Vehicle Catalog was born. Our journey
+        began with a small team of individuals who shared a deep love for
+        automobiles and a vision of making the process of buying and selling
+        vehicles easier than ever.
+      </p>
+
+      <h2>Our Mission</h2>
+      <p>
+        At Vehicle Catalog, our mission is to revolutionize the way people buy
+        and sell vehicles. We are committed to providing a user-friendly and
+        secure platform for both private sellers and dealerships. Our goal is to
+        empower our users with the tools and information they need to make
+        informed decisions. We believe in transparency, integrity, and
+        convenience. Whether you're looking for your first car, upgrading to a
+        family SUV, or expanding your collection of vintage classics, we are
+        here to make your journey smooth and memorable. Join us in our mission
+        to transform the world of vehicle trading.
+      </p>
+    </section>
+  );
+};
+
+export default About;
