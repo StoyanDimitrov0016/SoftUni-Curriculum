d2a778903b18b40435e891a71e4f79ff3c5fb8f0
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-31 18:05:29 +0200

    Relocate the svg icons for search and settings in public folder and set their corresponding links in NavigationBar component

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/src/components/Header/NavigationBar.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/src/components/Header/NavigationBar.jsx
@@ -1,37 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
-    return (
-        <nav className="navbar">
-            <h1>Welcome to the Vehicle Catalog</h1>
-            <ul>
-                <li>
-                    <a href="home.html">Home</a>
-                </li>
-                <li>
-                    <a href="login.html">Login</a>
-                </li>
-                <li>
-                    <a href="register.html">Register</a>
-                </li>
-                <li>
-                    <a href="about.html">About</a>
-                </li>
-                <li>
-                    <a href="search.html">
-                        {" "}
-                        <img src="/svg/search.svg" alt="search" />{" "}
-                    </a>
-                </li>
-                <li>
-                    <a href="settings.html">
-                        {" "}
-                        <img src="/svg/gear.svg" alt="settings" />{" "}
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav className="navbar">
+      <h1>Welcome to the Vehicle Catalog</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/about">About us</Link>
+        </li>
+        <li>
+          <Link to="/create-offer">Create an offer</Link>
+        </li>
+        <li>
+          <a href="search.html">
+            <img src="/icons/search.svg" alt="search" />
+          </a>
+        </li>
+        <li>
+          <a href="settings.html">
+            <img src="/icons/gear.svg" alt="settings" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavigationBar;
