46c9081cd958cdb8e0fd490580f7c0d40deb37f5
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-10-31 10:40:48 +0200

    Create Catalog and Footer components

---
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/src/components/Catalog/Catalog.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+const Catalog = () => {
+  return (
+    <section className="catalog">
+    <article className="vehicle-listing">
+      <img
+        src="https://www.bmw-m.com/content/dam/bmw/marketBMW_M/common/topics/magazine-article-pool/2018/bmw-m3-e46/bmw-e46-m3-coup%C3%A9-double-image-background.jpg"
+        alt="Car 1"
+      />
+      <h3>BMW E46</h3>
+      <p>Year: 2001</p>
+      <p>Price: $4,000</p>
+      <p>City: Your City Name</p>{" "}
+      {/* Replace "Your City Name" with the actual city name */}
+      <p>Fuel Type: Petrol</p>{" "}
+      {/* Replace "Petrol" with the actual fuel type */}
+      <p>Mileage: 100,000 miles</p>{" "}
+      {/* Replace "100,000 miles" with the actual mileage */}
+      <a href="details.html">View Details</a>
+    </article>
+    <article className="vehicle-listing">
+      <img
+        src="https://i.pinimg.com/1200x/e9/f7/af/e9f7af131e775a2aeb3de1ea113e780f.jpg"
+        alt="Car 2"
+      />
+      <h3>BMW E60</h3>
+      <p>Year: 2005</p>
+      <p>Price: $13,000</p>
+      <p>City: Your City Name</p>{" "}
+      {/* Replace "Your City Name" with the actual city name */}
+      <p>Fuel Type: Petrol</p>{" "}
+      {/* Replace "Petrol" with the actual fuel type */}
+      <p>Mileage: 100,000 miles</p>{" "}
+      {/* Replace "100,000 miles" with the actual mileage */}
+      <a href="details.html">View Details</a>
+    </article>
+    <article className="vehicle-listing">
+      <img
+        src="https://www.slashgear.com/img/gallery/10-reasons-the-bmw-e30-was-such-an-awesome-car/l-intro-1677525766.jpg"
+        alt="Car 3"
+      />
+      <h3>BMW E30</h3>
+      <p>Year: 1990</p>
+      <p>Price: $25,000</p>
+      <p>City: Your City Name</p>{" "}
+      {/* Replace "Your City Name" with the actual city name */}
+      <p>Fuel Type: Petrol</p>{" "}
+      {/* Replace "Petrol" with the actual fuel type */}
+      <p>Mileage: 100,000 miles</p>{" "}
+      {/* Replace "100,000 miles" with the actual mileage */}
+      <a href="details.html">View Details</a>
+    </article>
+    <article className="vehicle-listing">
+      <img
+        src="https://www.slashgear.com/img/gallery/10-reasons-the-bmw-e30-was-such-an-awesome-car/l-intro-1677525766.jpg"
+        alt="Car 3"
+      />
+      <h3>BMW E30</h3>
+      <p>Year: 1990</p>
+      <p>Price: $25,000</p>
+      <p>City: Your City Name</p>{" "}
+      {/* Replace "Your City Name" with the actual city name */}
+      <p>Fuel Type: Petrol</p>{" "}
+      {/* Replace "Petrol" with the actual fuel type */}
+      <p>Mileage: 100,000 miles</p>{" "}
+      {/* Replace "100,000 miles" with the actual mileage */}
+      <a href="details.html">View Details</a>
+    </article>
+  </section>
+  )
+}
+
+export default Catalog
