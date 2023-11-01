cdeb99c1ac927a774c843771d3fc988a4acc11de
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-01 20:59:33 +0200

    Create OfferPreview which will show the basic information  about the post

---
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Catalog/OfferPreview.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function OfferPreview({
+  id,
+  brand,
+  model,
+  image,
+  productionYear,
+  region,
+  price,
+  mileage,
+  fuelType,
+}) {
+  return (
+    <article className="vehicle-listing">
+      <img src={image} alt={`${brand} ${model}`} />
+      <h3>{`${brand} ${model}`}</h3>
+      <p>Year: {productionYear}</p>
+      <p>Price: ${price}</p>
+      <p>Region: {region}</p>
+      <p>Fuel Type: {fuelType}</p>
+      <p>Mileage: {mileage} miles</p>
+      <Link to={`/offer/derails/${id}`}>View Details</Link>
+    </article>
+  );
+}
+
+export default OfferPreview;
