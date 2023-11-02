dcc131c7fff97c0d7e1c6d45dcc4bd8ef706878e
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-02 20:39:29 +0200

    Add state for predefined options from the server
    Add useEffect to get the models of the chosen brand
    
    Add state for the created car configuration
    
    Add dynamically select tags rendering

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/CreateOffer/CreateOffer.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/CreateOffer/CreateOffer.jsx
@@ -1,54 +1,229 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import offerService from "../../services/offerService";
+import { useNavigate } from "react-router-dom";
+
+const keys = {
+  brand: "brand",
+  model: "model",
+  productionYear: "productionYear",
+  fuelType: "fuelType",
+  mileage: "mileage",
+  color: "color",
+  price: "price",
+  region: "region",
+  transmissionType: "transmissionType",
+  vehicleType: "vehicleType",
+  contactInformation: "contactInformation",
+  description: "description",
+  image: "image",
+};
 
 const CreateOffer = () => {
-  //Idea: to show the result before posting for further mitigating occurred mistakes
+  const [offerPredefinedOptions, setOfferPredefinedOptions] = useState({
+    [keys.brand]: [],
+    [keys.model]: [],
+    [keys.fuelType]: [],
+    [keys.color]: [],
+    [keys.region]: [],
+    [keys.transmissionType]: [],
+    [keys.vehicleType]: [],
+  });
+
+  const [offerValues, setOfferValues] = useState({
+    brand: "",
+    model: "",
+    productionYear: 0,
+    fuelType: "",
+    mileage: 0,
+    color: "",
+    price: 0,
+    region: "",
+    transmissionType: "",
+    vehicleType: "",
+    contactInformation: "",
+    description: "",
+    image: "",
+  });
+
+  useEffect(() => {
+    offerService
+      .getOfferOptions()
+      .then((options) => setOfferPredefinedOptions((state) => ({ ...state, ...options })))
+      .catch((error) =>
+        console.log("--- An error while fetching all offer options in CreateOffer occurred:", error)
+      );
+  }, []);
+
+  useEffect(() => {
+    offerService
+      .getBrandModels(offerValues.brand)
+      .then((models) => {
+        setOfferPredefinedOptions((state) => ({
+          ...state,
+          [keys.model]: models,
+        }));
+      })
+      .catch((error) =>
+        console.log("--- An error while fetching brand models in CreateOffer occurred:", error)
+      );
+  }, [offerValues.brand]);
+
+  const onChangeHandler = (e) => {
+    let { name, value } = e.target;
+
+    if (name === keys.productionYear || name === keys.mileage || name === keys.price) {
+      value = Number(value);
+    }
+
+    setOfferValues((state) => ({ ...state, [name]: value }));
+  };
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    await offerService.create(offerValues);
+    //TODO: navigate to my offers
+    navigate("/catalog");
+  };
+
   return (
-    <form className="new-offer-form">
+    <form className="new-offer-form" onSubmit={onSubmitHandler}>
       <label htmlFor="brand">Brand:</label>
-      <input type="text" id="brand" name="brand" required />
+      <select id="brand" name={keys.brand} required onChange={onChangeHandler}>
+        <option value="">Please select</option>
+        {offerPredefinedOptions.brand.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
 
       <label htmlFor="model">Model:</label>
-      <input type="text" id="model" name="model" required />
+      <select id="model" name={keys.model} required onChange={onChangeHandler}>
+        <option value="">Please select</option>
+        {offerPredefinedOptions.model.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
 
       <label htmlFor="year">Production year:</label>
-      <input type="number" id="year" name="year" required />
+      <input
+        type="number"
+        id="year"
+        name={keys.productionYear}
+        required
+        value={offerValues[keys.productionYear] || ""}
+        onChange={onChangeHandler}
+      />
 
       <label htmlFor="fuelType">Fuel Type:</label>
-      <select id="fuelType" name="fuelType" required>
-        <option value="Gasoline">Gasoline</option>
-        <option value="Diesel">Diesel</option>
-        <option value="Electric">Electric</option>
+      <select id="fuelType" name={keys.fuelType} required onChange={onChangeHandler}>
+        <option value="">Please select</option>
+        {offerPredefinedOptions.fuelType.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
 
       <label htmlFor="mileage">Mileage (in kilometers):</label>
-      <input type="number" id="mileage" name="mileage" required />
+      <input
+        type="number"
+        id="mileage"
+        name={keys.mileage}
+        required
+        value={offerValues.mileage || ""}
+        onChange={onChangeHandler}
+      />
 
       <label htmlFor="color">Color:</label>
-      <input type="text" id="color" name="color" required />
+      <select id="color" name={keys.color} required onChange={onChangeHandler}>
+        <option value="">Please select</option>
+        {offerPredefinedOptions.color.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
 
       <label htmlFor="price">Price:</label>
-      <input type="number" id="price" name="price" required />
+      <input
+        type="number"
+        id="price"
+        name={keys.price}
+        required
+        value={offerValues.price || ""}
+        onChange={onChangeHandler}
+      />
 
       <label htmlFor="region">Region:</label>
-      <input type="text" id="region" name="region" required />
+      <select id="region" name={keys.region} required onChange={onChangeHandler}>
+        <option value="">Please select</option>
+        {offerPredefinedOptions.region.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
 
       <label htmlFor="transmissionType">Transmission Type:</label>
-      <select id="transmissionType" name="transmissionType" required>
-        <option value="Automatic">Automatic</option>
-        <option value="Manual">Manual</option>
+      <select
+        id="transmissionType"
+        name={keys.transmissionType}
+        required
+        onChange={onChangeHandler}
+      >
+        <option value="">Please select</option>
+        {offerPredefinedOptions.transmissionType.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
 
       <label htmlFor="vehicleType">Vehicle Type:</label>
-      <input type="text" id="vehicleType" name="vehicleType" required />
+      <select id="vehicleType" name={keys.vehicleType} required onChange={onChangeHandler}>
+        <option value="">Please select</option>
+        {offerPredefinedOptions.vehicleType.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
 
       <label htmlFor="contactInformation">Contact Information:</label>
-      <input type="text" id="contactInformation" name="contactInformation" required />
+      <input
+        type="text"
+        id="contactInformation"
+        name={keys.contactInformation}
+        placeholder="tel. or email"
+        required
+        value={offerValues.contactInformation}
+        onChange={onChangeHandler}
+      />
 
       <label htmlFor="description">Description:</label>
-      <textarea id="description" name="description" required></textarea>
+      <textarea
+        id="description"
+        name={keys.description}
+        required
+        value={offerValues.description}
+        onChange={onChangeHandler}
+      ></textarea>
 
       <label htmlFor="image">Image:</label>
-      <input type="file" id="image" name="image" accept="image/*" required />
+      <input
+        type="text"
+        id="image"
+        name={keys.image}
+        placeholder="https://"
+        required
+        value={offerValues.image}
+        onChange={onChangeHandler}
+      />
 
       <button type="submit">Create Offer</button>
     </form>
