e78880b46119dcf41010cdd0bb1a7057d61ef405
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-03 22:11:21 +0200

    Add conditional rendering of the navigation bar depending if the user is logged in or he is just a quest

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Header/NavigationBar.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Header/NavigationBar.jsx
@@ -1,31 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 const NavigationBar = () => {
+  const { isAuthenticated } = useAuthContext();
+
   return (
     <nav className="navbar">
-      <h1>Welcome to the Vehicle Catalog</h1>
+      {/* <h1>Welcome to the Car Place</h1> */}
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/about">About us</Link>
-        </li>
-        <li>
-          <Link to="/create-offer">Create an offer</Link>
-        </li>
-        <li>
-          <a href="search.html">
-            <img src="/icons/search.svg" alt="search" />
-          </a>
-        </li>
+        {isAuthenticated ? (
+          <>
+            <li>
+              <Link to="/create-offer">Create an offer</Link>
+            </li>
+            <li>
+              <Link to="/search">
+                <img src="/icons/search.svg" alt="search" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/my-offers">My offers</Link>
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+
         <li>
           <a href="settings.html">
             <img src="/icons/gear.svg" alt="settings" />
