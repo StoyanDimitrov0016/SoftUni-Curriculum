c1e32399c9f9c1dd6231fc47a7333fa4d4988f78
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-03 22:03:46 +0200

    Relocate "about us" link to be in the Footer instead in the navigation bar

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Footer/Footer.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
       <p>© 2023 Vehicle Catalog</p>
+      <li>
+        <Link to="/about">About us</Link>
+      </li>
     </footer>
   );
 };
