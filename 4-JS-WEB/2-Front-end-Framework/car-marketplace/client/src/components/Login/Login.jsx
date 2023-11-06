35366f0d8c6d8255cc44ca8860dd1ccabecb56ca
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-06 22:24:10 +0200

    Refactor Login component to use useForm custom hook

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Login/Login.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Login/Login.jsx
@@ -1,30 +1,49 @@
-import React from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useForm } from "../../hooks/useForm";
 
 const Login = () => {
+  const { onLoginSubmit } = useAuth();
+
+  const { formValues, changeHandler, onSubmit } = useForm(
+    {
+      email: "",
+      password: "",
+    },
+    onLoginSubmit
+  );
+
   return (
-    <>
-      <form action="/login" method="post">
-        <label htmlFor="userType">Select User Type:</label>
-        <select name="userType" id="userType">
-          <option value="regular">Regular User</option>
-          <option value="company">Company</option>
-        </select>
-        <div className="common-fields">
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" required="" />
-        </div>
-        <div className="company-fields">
-          <label htmlFor="companyName">Company Name:</label>
-          <input type="text" name="companyName" id="companyName" />
-        </div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" required="" />
-        <button type="submit">Login</button>
-      </form>
-      <p>
-        Don't have an account? <a href="/register.html">Register</a>
-      </p>
-    </>
+    <form className="login-form" onSubmit={onSubmit}>
+      <label htmlFor="email" className="login-label">
+        Email:
+      </label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={formValues.email}
+        onChange={changeHandler}
+        className="login-input"
+        required
+      />
+
+      <label htmlFor="password" className="login-label">
+        Password:
+      </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={formValues.password}
+        onChange={changeHandler}
+        className="login-input"
+        required
+      />
+
+      <button type="submit" className="login-button">
+        Login
+      </button>
+    </form>
   );
 };
 
