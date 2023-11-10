5130fca4261960c1039036437d0a8b201adfd9c0
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-10 23:13:09 +0200

    Remove unnecessary import

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Catalog/Catalog.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Catalog/Catalog.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import offerService from "../../services/offerService";
 import OfferPreview from "./OfferPreview";
 
