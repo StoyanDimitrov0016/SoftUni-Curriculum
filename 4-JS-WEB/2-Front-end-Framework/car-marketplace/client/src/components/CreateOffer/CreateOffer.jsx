d9cb8b506cc5b7d2a75f599f1caca8807353fe83
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-12 21:29:13 +0200

    Add additional logic if the user is a dealership to make a second request setting the offer id to his entry in dealerships to persist the data there due to the server's limitation.

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/CreateOffer/CreateOffer.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/CreateOffer/CreateOffer.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
 import offerService from "../../services/offerService";
-import { useForm } from "../../hooks/useForm";
+
 import { useNavigate } from "react-router-dom";
 import { useCarOfferForm } from "../../hooks/useCarOfferForm";
 import CarOfferForm from "../CarOfferForm/CarOfferForm";
+import { useAuthContext } from "../../contexts/AuthContext";
+import dealershipService from "../../services/dealershipService";
 
 const CreateOffer = () => {
   const navigate = useNavigate();
+  const { userCredentials } = useAuthContext();
+  const { reference, userType } = userCredentials;
 
   const onSubmitHandler = async () => {
     try {
-      await offerService.create(formValues);
+      const offerData = await offerService.create(formValues);
+
+      if (userType === "dealership") {
+        const result = await dealershipService.addOfferIdToAvailableOffers(
+          reference,
+          offerData._id
+        );
+        console.log(result);
+      }
+
       navigate("/my-offers");
     } catch (error) {
       console.log("An error while creating an offer occurred in CreateOffer", error);
