469dd07c74a24da1cc2203fd382bc2e0d451c7be
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-12 21:22:08 +0200

    Remove the unnecessary functions - login, register and logout - they will be provided from authenticationService

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/services/dealershipService.js
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/services/dealershipService.js
@@ -1,65 +1,41 @@
 import requestHTTP from "./requestHTTP";
-
-const dealershipEndpoints = {
-    login: '/users/login',
-    register: '/users/register',
-    dealershipCollection: '/data/dealerships',
-    getDealershipEntity: (dealershipId) => `/data/dealerships/${dealershipId}`,
-    logout: '/users/logout',
-    deleteOffer: (offerId) => `/data/offers/${offerId}`
-};
-
-async function login(email, password) {
-    const userCredentials = await requestHTTP.post(dealershipEndpoints.login, { email, password });
-    return userCredentials;
-}
-
-async function register({ dealershipName, email, password, userType }) {
-    const userCredentials = await requestHTTP.post(dealershipEndpoints.register, { dealershipName, email, password, userType });
-    return userCredentials;
-}
+import { dealershipEndpoints as endpoints } from "./endpoints";
 
 async function createDealershipInCollection({ dealershipName, location, phoneNumber, email, workingHours }) {
     const availableOffers = [];
-    const createdEntry = await requestHTTP.post(dealershipEndpoints.dealershipCollection, { dealershipName, location, phoneNumber, email, workingHours, availableOffers });
+    const createdEntry = await requestHTTP.post(endpoints.dealershipsCollection,
+        {
+            dealershipName,
+            location,
+            phoneNumber,
+            email,
+            workingHours,
+            availableOffers
+        });
     return createdEntry;
 }
 
 async function addOfferIdToAvailableOffers(dealershipId, offerId) {
-    const dealershipEntity = await requestHTTP.get(dealershipEndpoints.getDealershipEntity(dealershipId));
+    const dealershipEntity = await requestHTTP.get(endpoints.specificDealership(dealershipId));
     dealershipEntity.availableOffers.push(offerId);
 
-    const result = await requestHTTP.put(dealershipEndpoints.getDealershipEntity(dealershipId), dealershipEntity);
+    const result = await requestHTTP.put(endpoints.specificDealership(dealershipId), dealershipEntity);
     return result;
 }
-
-// async function deleteOffer(offerId) {
-//     const result = await requestHTTP.delete(dealershipEndpoints.deleteOffer(offerId));
-//     return result;
-// }
 
 async function removeOfferIdFromAvailableOffers(dealershipId, offerId) {
-    const dealershipEntity = await requestHTTP.get(dealershipEndpoints.getDealershipEntity(dealershipId));
+    const dealershipEntity = await requestHTTP.get(endpoints.specificDealership(dealershipId));
 
-    const availableOffers = dealershipEntity.availableOffers.filter((id) => id !== offerId);
-    dealershipEntity.availableOffers = availableOffers;
+    const filteredAvailableOffers = dealershipEntity.availableOffers.filter((id) => id !== offerId);
+    dealershipEntity.availableOffers = filteredAvailableOffers;
 
-    const result = await requestHTTP.put(dealershipEndpoints.getDealershipEntity(dealershipId), dealershipEntity);
-    return result;
-}
-
-async function logout() {
-    const result = await requestHTTP.get(dealershipEndpoints.logout);
+    const result = await requestHTTP.put(endpoints.specificDealership(dealershipId), dealershipEntity);
     return result;
 }
 
 const dealershipService = {
-    login,
-    register,
-    logout,
     createDealershipInCollection,
     addOfferIdToAvailableOffers,
-    // deleteOffer,
     removeOfferIdFromAvailableOffers
 };
 
