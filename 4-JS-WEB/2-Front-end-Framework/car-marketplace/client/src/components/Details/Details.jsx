989e3e115f73e5674e02bc98123103a7e9cfc445
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-12 21:34:34 +0200

    Refactor Details component to enhance modularity and readability:
    - Extract delete confirmation logic into a separate state and handlers.
    - Integrate watchlist functionality, separating it from the main component logic.
    - Improve code organization and structure.

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Details/Details.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Details/Details.jsx
@@ -1,39 +1,95 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
 import offerService from "../../services/offerService";
 import DetailsPreview from "./DetailsPreview";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 const Details = () => {
-  const [offer, setOffer] = useState({
-    _id: "",
-    _ownerId: "",
-    brand: "",
-    model: "",
-    productionYear: 0,
-    fuelType: "",
-    mileage: 0,
-    color: "",
-    price: 0,
-    region: "",
-    transmissionType: "",
-    vehicleType: "",
-    contactInformation: "",
-    description: "",
-    image: "",
-  });
+  const [offer, setOffer] = useState({});
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const navigate = useNavigate();
 
   const { offerId } = useParams();
+  const { userCredentials } = useAuthContext();
+  const { userId } = userCredentials;
+
+  const deleteClickHandler = () => {
+    setIsDeleting(true);
+  };
+
+  const cancelDeleteClickHandler = () => {
+    setIsDeleting(false);
+  };
+
+  const confirmDeleteClickHandler = async () => {
+    try {
+      await offerService.delete(offerId);
+      navigate("/my-offers");
+    } catch (error) {
+      console.error("Error deleting offer:", error);
+    }
+  };
+
+  const addToWatchList = async () => {
+    await offerService.addToWatchList(offerId);
+    const watchlistCount = await offerService.getWatchlistCountForOffer(offerId);
+
+    setOffer((state) => ({
+      ...state,
+      canAdd: !state.canAdd,
+      watchlistCount,
+    }));
+  };
+
+  const removeFromWatchList = async () => {
+    await offerService.removeFromWatchList(offerId, userId);
+    const watchlistCount = await offerService.getWatchlistCountForOffer(offerId);
+
+    setOffer((state) => ({
+      ...state,
+      canAdd: !state.canAdd,
+      watchlistCount,
+    }));
+  };
 
   useEffect(() => {
-    offerService
-      .getOne(offerId)
-      .then((result) => setOffer(result))
-      .catch((error) =>
-        console.log("--- An error while fetching current offer in OfferPreview occurred:", error)
-      );
-  }, [offerId]);
+    const fetchOfferData = async () => {
+      try {
+        const [fetchedOffer, watchlistCount, canAdd] = await Promise.all([
+          offerService.getOne(offerId),
+          offerService.getWatchlistCountForOffer(offerId),
+          offerService.canAddToWatchlist(offerId, userId),
+        ]);
 
-  return <DetailsPreview {...offer} />;
+        const isOwner = fetchedOffer._ownerId === userId;
+
+        setOffer({
+          ...fetchedOffer,
+          watchlistCount,
+          canAdd,
+          isOwner,
+        });
+      } catch (error) {
+        console.error("Error fetching offer data:", error);
+      }
+    };
+
+    fetchOfferData();
+  }, [offerId, userId]);
+
+  return (
+    <DetailsPreview
+      offer={offer}
+      isDeleting={isDeleting}
+      deleteClickHandler={deleteClickHandler}
+      cancelDeleteClickHandler={cancelDeleteClickHandler}
+      confirmDeleteClickHandler={confirmDeleteClickHandler}
+      addToWatchList={addToWatchList}
+      removeFromWatchList={removeFromWatchList}
+    />
+  );
 };
 
 export default Details;
