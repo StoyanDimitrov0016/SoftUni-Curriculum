f03c265045bf2f0f44216b92135bbebca2bb37b3
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-12 21:20:45 +0200

    Creating "endpoints" file which will store all the needed endpoints in the services.

---
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/services/endpoints.js
@@ -0,0 +1,31 @@
+const authenticationEndpoints = {
+    login: '/users/login',
+    register: '/users/register',
+    logout: '/users/logout',
+};
+
+const dealershipEndpoints = {
+    dealershipsCollection: '/data/dealerships',
+    specificDealership: (dealershipId) => `/data/dealerships/${dealershipId}`,
+    deleteOffer: (offerId) => `/data/offers/${offerId}`
+};
+
+const offerEndpoints = {
+    offersCollection: '/data/offers',
+    specificOffer: (id) => `/data/offers/${id}`,
+    vehicleOptions: '/data/vehicle-properties',
+    brandModels: (brand) => `/data/vehicle-models/${brand}`,
+    userOffers: (id) => {
+        const staticPart = '/data/offers/?where=';
+        const encodedPart = encodeURIComponent(`_ownerId="${id}"`);
+        return staticPart + encodedPart;
+    },
+    watchlistsCollection: '/data/watchlists',
+    specificWatchlistById: (watchlistId) => `/data/watchlists/${watchlistId}`,
+    specificWatchlistByOfferAndUserId: (offerId, userId) => `/data/watchlists?where=offerId%3D%22${offerId}%22%20and%20_ownerId%3D%22${userId}%22`,
+    specificWatchlistCount: (offerId) => `/data/watchlists?where=offerId%3D%22${offerId}%22&count`,
+    checkWatchlist: (offerId, userId) =>
+        `/data/watchlists?where=offerId%3D%22${offerId}%22%20and%20_ownerId%3D%22${userId}%22`
+};
+
+export { authenticationEndpoints, dealershipEndpoints, offerEndpoints };
