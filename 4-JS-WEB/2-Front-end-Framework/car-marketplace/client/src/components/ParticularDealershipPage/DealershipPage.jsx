da1d33d0006d64d1a024fdbb4ca0d5bc5e48e4e1
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-18 17:49:31 +0200

    Extended the DealershipPage component to include the ability to load comments, handle user reviews, and added corresponding states and functions.
    - Added states: `reviews`, `userReview`.
    - Added functions: `loadComments`, `changeHandler`, `submit`, `addComment`, `addStarReview`.
    - Load comments using `loadComments` on component mount.
    - Implemented a user review form with input fields for stars and comments.

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/ParticularDealershipPage/DealershipPage.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/ParticularDealershipPage/DealershipPage.jsx
@@ -9,18 +9,36 @@ const DealershipPage = () => {
 
   const [dealerInfo, setDealerInfo] = useState({});
   const [currentOffers, setCurrentOffers] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [userReview, setUserReview] = useState({ stars: 0, comment: "" });
 
   const loadAvailableOffers = async (availableOffers) => {
-      for (let i = 0; i < availableOffers.length; i++) {
-        const offerId = availableOffers[i];
+    for (let i = 0; i < availableOffers.length; i++) {
+      const offerId = availableOffers[i];
 
-        try {
-          const offer = await offerService.getOne(offerId);
-          setCurrentOffers((state) => [...state, offer]);
-        } catch (error) {
-          console.log("Error while fetching offer:", error);
-        }
+      try {
+        const offer = await offerService.getOne(offerId);
+        setCurrentOffers((state) => [...state, offer]);
+      } catch (error) {
+        console.log("Error while fetching offer:", error);
       }
+    }
+  };
+
+  const loadComments = async () => {
+    const fetchedReviews = await dealershipService.getReviewsForSpecificDealership(dealershipId);
+    console.log(fetchedReviews);
+    setReviews([...fetchedReviews]);
+  };
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+
+    setUserReview((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
   };
 
   const addComment = async () => {};
@@ -33,7 +51,8 @@ const DealershipPage = () => {
 
         setDealerInfo(receivedDealership);
         // debugger
-         await loadAvailableOffers(receivedDealership.availableOffers);
+        await loadAvailableOffers(receivedDealership.availableOffers);
+        await loadComments();
       } catch (error) {
         console.log("Error while fetching dealership info", error);
       }
@@ -48,6 +67,9 @@ const DealershipPage = () => {
     <DealershipPreview
       dealershipInfo={dealerInfo}
       currentOffers={currentOffers}
+      reviews={reviews}
+      userReview={userReview}
+      changeHandler={changeHandler}
       addComment={addComment}
       addStarReview={addStarReview}
     />
