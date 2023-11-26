40c6ba8cfa5a176309eb2101a44f0cff2a8050fc
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-26 20:18:31 +0200

    Create a separate file for storing the protected path

---
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/Paths/protectedPaths.js
@@ -0,0 +1,44 @@
+const PROTECTED_ROUTES = {
+    DETAILS: {
+        label: "Details",
+        dynamicPath: (offerId) => `/offer/${offerId}`,
+        staticPath: '/offer/:offerId',
+    },
+    CREATE_OFFER: {
+        label: "Create Offer",
+        staticPath: '/create-offer',
+    },
+    MY_OFFERS: {
+        label: "My Offers",
+        staticPath: '/my-offers',
+    },
+    WATCHLIST: {
+        label: "My Watchlist",
+        staticPath: '/my-watchlist',
+    },
+    LOGOUT: {
+        label: "Logout",
+        staticPath: '/logout',
+    },
+    EDIT_OFFER: {
+        label: "Edit Offer",
+        dynamicPath: (offerId) => `/offer/edit/${offerId}`,
+        staticPath: '/offer/edit/:offerId',
+    },
+    DELETE_OFFER: {
+        label: "Delete Offer",
+        dynamicPath: (offerId) => `/offer/delete/${offerId}`,
+        staticPath: '/offer/delete/:offerId',
+    },
+    DEALERSHIP_PAGE: {
+        label: "Dealership Page",
+        dynamicPath: (dealershipId) => `/dealerships/${dealershipId}`,
+        staticPath: '/dealerships/:dealershipId',
+    },
+    SEARCH: {
+        label: "Search",
+        staticPath: '/search',
+    },
+};
+
+export default PROTECTED_ROUTES;
