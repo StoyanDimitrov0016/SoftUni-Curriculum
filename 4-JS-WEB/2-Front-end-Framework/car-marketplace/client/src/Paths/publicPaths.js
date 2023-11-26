5ea23fe008a7a305850307a92861de1ae65eb84e
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-11-26 20:19:01 +0200

    Create a separate file for storing the public path

---
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/Paths/publicPaths.js
@@ -0,0 +1,20 @@
+const PUBLIC_ROUTES = {
+    HOME: {
+        label: "Home",
+        path: '/'
+    },
+    LOGIN: {
+        label: "Login",
+        path: '/login'
+    },
+    REGISTER: {
+        label: "Register",
+        path: '/register'
+    },
+    ABOUT: {
+        label: "About",
+        path: '/about'
+    }
+};
+
+export default PUBLIC_ROUTES;
