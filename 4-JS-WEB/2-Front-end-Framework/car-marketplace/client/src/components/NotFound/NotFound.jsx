82113f6cb303590926bb1b34d025c6d1b062ae24
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-12-04 19:16:14 +0200

    feat(not-found): create 404 Not Found page

---
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found-title">404 - Not Found</h1>
+      <p className="not-found-message">
+        The page you are looking for cannot be found.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
