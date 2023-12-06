e661b48065ddf54dcd9bbf68292cc501496232b8
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-12-06 23:25:04 +0200

    feat(pagination): create Pagination component and integrate it into Catalog component

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Catalog/Catalog.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Catalog/Catalog.jsx
@@ -1,24 +1,60 @@
 import { useEffect, useState } from "react";
-
 import offerService from "../../services/offerService";
 import OfferPreview from "./OfferPreview";
+import Pagination from "../Pagination/Pagination";
 
 const Catalog = () => {
+  const PAGE_SIZE = 6;
+
   const [offers, setOffers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [offerCount, setOfferCount] = useState(0);
 
   useEffect(() => {
-    offerService
-      .getAll()
-      .then((result) => setOffers(result))
-      .catch((error) =>
-        console.log("--- An error while fetching all offers in Catalog occurred:", error)
-      );
-  }, []);
+    const fetchData = async () => {
+      try {
+        const [fetchedOffers, count] = await Promise.all([
+          offerService.fetchPaginatedEntities("offers", currentPage, PAGE_SIZE),
+          offerService.getOfferCount(),
+        ]);
+
+        setOffers(fetchedOffers);
+        setOfferCount(count);
+      } catch (error) {
+        console.error("Error fetching offers in Catalog:", error);
+      }
+    };
+
+    fetchData();
+  }, [currentPage]);
+
+  const onPageChange = (action) => {
+    if (action === "reduce" && currentPage > 1) {
+      setCurrentPage((state) => state - 1);
+    } else if (action === "increase" && currentPage < Math.ceil(offerCount / PAGE_SIZE)) {
+      setCurrentPage((state) => state + 1);
+    }
+  };
+
+  const setParticularPage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <section className="catalog">
       {offers.length > 0 ? (
-        offers.map((offer) => <OfferPreview key={offer._id} {...offer} />)
+        <>
+          {offers.map((offer) => (
+            <OfferPreview key={offer._id} {...offer} />
+          ))}
+
+          <Pagination
+            currentPage={currentPage}
+            linkCount={Math.ceil(offerCount / PAGE_SIZE)}
+            onPageChange={onPageChange}
+            setParticularPage={setParticularPage}
+          />
+        </>
       ) : (
         <p>There are no offers yet</p>
       )}
