fccaca5c11b808f064968e131ce2cb9106661669
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-12-09 00:05:36 +0200

    style: Update copyright year dynamically in Footer component

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Footer/Footer.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <p>© 2023 Vehicle Catalog</p>
+      <p>© {new Date().getFullYear()} Vehicle Catalog</p>
       <ul>
         <li>
           <Link to="/about">About us</Link>
