f94dabb3f327a2dde93a687e3c93bf8b0b2070e5
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-12-09 00:13:53 +0200

    feat(pagination): refactor and optimize pagination component
    - Consolidate the logic for calculating page count and handling page changes
    - Rename the prop onPageChange to setCurrentPage for consistency
    - Improve code readability and maintainability

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Pagination/Pagination.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Pagination/Pagination.jsx
@@ -1,56 +1,69 @@
-const Pagination = ({ currentPage, linkCount, onPageChange, setParticularPage }) => {
-    const pageLinks = [];
-  
-    const buttonStyle = {
-      cursor: "pointer",
-      padding: "8px 12px",
-      margin: "0 4px",
-      borderRadius: "4px",
-      border: "1px solid #ccc",
-      background: "#fff",
-      color: "#333",
-      textDecoration: "none",
-      display: "inline-block",
-    };
-  
-    for (let i = 1; i <= linkCount; i++) {
-      const li = (
-        <li
-          key={i}
-          className={i === currentPage ? "paginationLink active" : "paginationLink"}
-          style={buttonStyle}
-          onClick={() => setParticularPage(i)}
-        >
-          {i}
-        </li>
-      );
-      pageLinks.push(li);
-    }
-  
-    return (
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        {currentPage > 1 && (
-          <li
-            className="paginationLink"
-            style={{ ...buttonStyle, fontWeight: "bold" }}
-            onClick={() => onPageChange("reduce")}
-          >
-            &laquo; Previous
-          </li>
-        )}
-        {pageLinks}
-        {currentPage < linkCount && (
-          <li
-            className="paginationLink"
-            style={{ ...buttonStyle, fontWeight: "bold" }}
-            onClick={() => onPageChange("increase")}
-          >
-            Next &raquo;
-          </li>
-        )}
-      </ul>
-    );
+const Pagination = ({ currentPage, setCurrentPage, offerCount, PAGE_SIZE }) => {
+  const pageLinks = [];
+
+  const buttonStyle = {
+    cursor: "pointer",
+    padding: "8px 12px",
+    margin: "0 4px",
+    borderRadius: "4px",
+    border: "1px solid #ccc",
+    background: "#fff",
+    color: "#333",
+    textDecoration: "none",
+    display: "inline-block",
   };
-  
-  export default Pagination;
-  
+
+  const linkCount = Math.ceil(offerCount / PAGE_SIZE);
+
+  const onPageChange = (action) => {
+    if (action === "reduce" && currentPage > 1) {
+      setCurrentPage((state) => state - 1);
+    } else if (action === "increase" && currentPage < linkCount) {
+      setCurrentPage((state) => state + 1);
+    }
+  };
+
+  const setParticularPage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  for (let i = 1; i <= linkCount; i++) {
+    const li = (
+      <li
+        key={i}
+        className={i === currentPage ? "paginationLink active" : "paginationLink"}
+        style={buttonStyle}
+        onClick={() => setParticularPage(i)}
+      >
+        {i}
+      </li>
+    );
+    pageLinks.push(li);
+  }
+
+  return (
+    <ul style={{ listStyle: "none", padding: 0 }}>
+      {currentPage > 1 && (
+        <li
+          className="paginationLink"
+          style={{ ...buttonStyle, fontWeight: "bold" }}
+          onClick={() => onPageChange("reduce")}
+        >
+          &laquo; Previous
+        </li>
+      )}
+      {pageLinks}
+      {currentPage < linkCount && (
+        <li
+          className="paginationLink"
+          style={{ ...buttonStyle, fontWeight: "bold" }}
+          onClick={() => onPageChange("increase")}
+        >
+          Next &raquo;
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default Pagination;
