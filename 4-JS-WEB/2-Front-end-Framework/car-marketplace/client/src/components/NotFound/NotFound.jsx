144e5c082ff6682339417f9a13f66776d9079486
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-12-09 00:13:05 +0200

    feat(not-found): enhance 404 page with navigation
    - Add link to the home page on the NotFound component
    - Import Link and publicRoutes for navigation
    - Update component structure to include the new link

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/NotFound/NotFound.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/NotFound/NotFound.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PUBLIC_ROUTES from "../../Routes/publicRoutes";
 
 const NotFound = () => {
   return (
-    <div className="not-found-container">
-      <h1 className="not-found-title">404 - Not Found</h1>
-      <p className="not-found-message">
-        The page you are looking for cannot be found.
-      </p>
-    </div>
+    <>
+      <div className="not-found-container">
+        <h1 className="not-found-title">404 - Not Found</h1>
+        <p className="not-found-message">The page you are looking for cannot be found.</p>
+      </div>
+      <Link to={PUBLIC_ROUTES.HOME.path}>Go to home</Link>
+    </>
   );
 };
 
