511d7eab47bc5fad2462a00f82fffaa08ef8642d
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-12-09 00:33:04 +0200

    refactor(search): separate form logic into SearchForm component
    - Move the form logic, including form rendering and handling, to a new component called SearchForm.
    - Introduce a loading state (isLoading) to provide better UI feedback during the search process.
    - Display a loading message while waiting for search results.
    - Improve code structure and readability by breaking down the component into smaller, focused parts.

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Search/Search.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Search/Search.jsx
@@ -1,26 +1,18 @@
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { useCarOfferForm } from "../../hooks/useCarOfferForm";
 import searchService from "../../services/searchService";
 
-import BrandSelect from "../CarOfferForm/Selects/BrandSelect";
-import ModelSelect from "../CarOfferForm/Selects/ModelSelect";
-import ProductionYearInput from "../CarOfferForm/Inputs/ProductionYearInput";
-import FuelTypeSelect from "../CarOfferForm/Selects/FuelTypeSelect";
-import ColorSelect from "../CarOfferForm/Selects/ColorSelect";
-import RegionSelect from "../CarOfferForm/Selects/RegionSelect";
-import TransmissionTypeSelect from "../CarOfferForm/Selects/TransmissionTypeSelect";
-import PriceInput from "../CarOfferForm/Inputs/PriceInput";
-import VehicleTypeSelect from "../CarOfferForm/Selects/VehicleTypeSelect";
-import MileageInput from "../CarOfferForm/Inputs/MileageInput";
+import SearchForm from "./SearchForm";
 import OfferPreview from "../Catalog/OfferPreview";
-import { useLocation, useNavigate } from "react-router-dom";
 
 const Search = () => {
   const navigate = useNavigate();
   const locationSearch = useLocation();
 
   const [matches, setMatches] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   const queryString = locationSearch.search.slice(1);
   const searchParameters = queryString.split("&").map((qs) => qs.split("="));
@@ -48,82 +40,26 @@ const Search = () => {
     searchService
       .search(searchParameters)
       .then((result) => setMatches(result))
-      .catch((error) => console.log("Error while fetching offers in search page:", error));
+      .catch((error) => console.log("Error while fetching offers in search page:", error))
+      .finally(() => setLoading(false));
 
     setFormValues((state) => ({ ...state, ...Object.fromEntries(searchParameters) }));
-  }, [locationSearch]);
+  }, []);
 
   return (
     <>
-      <form className="search-form" onSubmit={submit}>
-        <BrandSelect
-          brands={offerPredefinedOptions.brand}
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <SearchForm
+          offerPredefinedOptions={offerPredefinedOptions}
+          formValues={formValues}
           changeHandler={changeHandler}
-          selectedBrand={formValues.brand}
-          isRequired={false}
+          submit={submit}
+          resetFormValues={resetFormValues}
+          isLoading={isLoading}
         />
-
-        <ModelSelect
-          models={offerPredefinedOptions.model}
-          changeHandler={changeHandler}
-          selectedModel={formValues.model}
-          isRequired={false}
-        />
-
-        <ProductionYearInput
-          productionYear={formValues.productionYear}
-          changeHandler={changeHandler}
-          isRequired={false}
-        />
-
-        <FuelTypeSelect
-          fuelTypes={offerPredefinedOptions.fuelType}
-          changeHandler={changeHandler}
-          selectedFuelType={formValues.fuelType}
-          isRequired={false}
-        />
-
-        <MileageInput
-          mileage={formValues.mileage}
-          changeHandler={changeHandler}
-          isRequired={false}
-        />
-
-        <ColorSelect
-          colors={offerPredefinedOptions.color}
-          changeHandler={changeHandler}
-          selectedColor={formValues.color}
-          isRequired={false}
-        />
-
-        <PriceInput price={formValues.price} changeHandler={changeHandler} isRequired={false} />
-
-        <RegionSelect
-          regions={offerPredefinedOptions.region}
-          changeHandler={changeHandler}
-          selectedRegion={formValues.region}
-          isRequired={false}
-        />
-
-        <TransmissionTypeSelect
-          transmissionTypes={offerPredefinedOptions.transmissionType}
-          changeHandler={changeHandler}
-          selectedTransmissionType={formValues.transmissionType}
-          isRequired={false}
-        />
-
-        <VehicleTypeSelect
-          vehicleTypes={offerPredefinedOptions.vehicleType}
-          changeHandler={changeHandler}
-          selectedVehicleType={formValues.vehicleType}
-          isRequired={false}
-        />
-
-        <button className="clear" onClick={resetFormValues}>
-          Clear
-        </button>
-        <button type="submit">Search</button>
-      </form>
+      )}
 
       {matches.length >= 1 ? (
         <section className="content-container">
