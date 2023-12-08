2c0f2f87b85651edc3c585bafc768a0aaf66c49f
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-12-09 00:35:16 +0200

    refactor(search): create SearchForm component
    - Extract the search form logic into a new component called SearchForm.
    - Improve code organization by grouping related form elements.
    - Introduce loading state (isLoading) to provide better UI feedback during the search process.
    - Use conditional rendering on the "Search" button to display "Searching..." while waiting for results.
    - Enhance readability and maintainability by breaking down the form structure into individual components.

---
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/components/Search/SearchForm.jsx
@@ -0,0 +1,91 @@
+import MileageInput from "../CarOfferForm/Inputs/MileageInput";
+import PriceInput from "../CarOfferForm/Inputs/PriceInput";
+import ProductionYearInput from "../CarOfferForm/Inputs/ProductionYearInput";
+import BrandSelect from "../CarOfferForm/Selects/BrandSelect";
+import ColorSelect from "../CarOfferForm/Selects/ColorSelect";
+import FuelTypeSelect from "../CarOfferForm/Selects/FuelTypeSelect";
+import ModelSelect from "../CarOfferForm/Selects/ModelSelect";
+import RegionSelect from "../CarOfferForm/Selects/RegionSelect";
+import TransmissionTypeSelect from "../CarOfferForm/Selects/TransmissionTypeSelect";
+import VehicleTypeSelect from "../CarOfferForm/Selects/VehicleTypeSelect";
+
+const SearchForm = ({
+  offerPredefinedOptions,
+  formValues,
+  changeHandler,
+  submit,
+  resetFormValues,
+  isLoading,
+}) => {
+  return (
+    <form className="search-form" onSubmit={submit}>
+      <BrandSelect
+        brands={offerPredefinedOptions.brand}
+        changeHandler={changeHandler}
+        selectedBrand={formValues.brand}
+        isRequired={false}
+      />
+
+      <ModelSelect
+        models={offerPredefinedOptions.model}
+        changeHandler={changeHandler}
+        selectedModel={formValues.model}
+        isRequired={false}
+      />
+
+      <ProductionYearInput
+        productionYear={formValues.productionYear}
+        changeHandler={changeHandler}
+        isRequired={false}
+      />
+
+      <FuelTypeSelect
+        fuelTypes={offerPredefinedOptions.fuelType}
+        changeHandler={changeHandler}
+        selectedFuelType={formValues.fuelType}
+        isRequired={false}
+      />
+
+      <MileageInput mileage={formValues.mileage} changeHandler={changeHandler} isRequired={false} />
+
+      <ColorSelect
+        colors={offerPredefinedOptions.color}
+        changeHandler={changeHandler}
+        selectedColor={formValues.color}
+        isRequired={false}
+      />
+
+      <PriceInput price={formValues.price} changeHandler={changeHandler} isRequired={false} />
+
+      <RegionSelect
+        regions={offerPredefinedOptions.region}
+        changeHandler={changeHandler}
+        selectedRegion={formValues.region}
+        isRequired={false}
+      />
+
+      <TransmissionTypeSelect
+        transmissionTypes={offerPredefinedOptions.transmissionType}
+        changeHandler={changeHandler}
+        selectedTransmissionType={formValues.transmissionType}
+        isRequired={false}
+      />
+
+      <VehicleTypeSelect
+        vehicleTypes={offerPredefinedOptions.vehicleType}
+        changeHandler={changeHandler}
+        selectedVehicleType={formValues.vehicleType}
+        isRequired={false}
+      />
+
+      <button className="clear" onClick={resetFormValues}>
+        Clear
+      </button>
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? "Searching..." : "Search"}
+      </button>
+    </form>
+  );
+};
+
+export default SearchForm;
