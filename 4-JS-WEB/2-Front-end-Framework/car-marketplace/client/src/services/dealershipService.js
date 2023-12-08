414554ec2befb46b9dd5c41dd0009f1b834a822b
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-12-09 00:42:38 +0200

    refactor(service): update dealership service endpoints
    - Update the dealership service to use new endpoints for dealership-related operations.
    - Replace the old searchEndpoints import with the updated dealershipEndpoints.
    - Update the endpoint references in the service functions accordingly for consistency.
    - Enhance code readability and maintainability by using the new endpoints structure.

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/services/dealershipService.js
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/services/dealershipService.js
@@ -1,5 +1,5 @@
 import requestHTTP from "./requestHTTP";
-import { dealershipEndpoints as endpoints, searchEndpoints } from "./endpoints";
+import { dealershipEndpoints as endpoints } from "./endpoints";
 
 async function createDealershipInCollection({ dealershipName, location, phoneNumber, email, workingHours }) {
     const availableOffers = [];
@@ -16,33 +16,32 @@ async function createDealershipInCollection({ dealershipName, location, phoneNum
 }
 
 async function getDealershipEntity(dealershipId) {
-    const dealershipEntity = requestHTTP.get(endpoints.specificDealership(dealershipId));
+    const dealershipEntity = await requestHTTP.get(endpoints.specificDealershipById(dealershipId));
     return dealershipEntity;
 }
 
 async function getReference(userId) {
-    //TODO: add proper endpoint for getting this reference
-    const dealershipEntity = await requestHTTP.get(`/data/dealerships?where=_ownerId%3D%22${userId}%22`);
+    const dealershipEntity = await requestHTTP.get(endpoints.specificDealershipByQuery(userId));
 
     const reference = dealershipEntity[0]._id;
     return reference;
 }
 
 async function addOfferIdToAvailableOffers(dealershipId, offerId) {
-    const dealershipEntity = await requestHTTP.get(endpoints.specificDealership(dealershipId));
+    const dealershipEntity = await requestHTTP.get(endpoints.specificDealershipById(dealershipId));
     dealershipEntity.availableOffers.push(offerId);
 
-    const result = await requestHTTP.put(endpoints.specificDealership(dealershipId), dealershipEntity);
+    const result = await requestHTTP.put(endpoints.specificDealershipById(dealershipId), dealershipEntity);
     return result;
 }
 
 async function removeOfferIdFromAvailableOffers(dealershipId, offerId) {
-    const dealershipEntity = await requestHTTP.get(endpoints.specificDealership(dealershipId));
+    const dealershipEntity = await requestHTTP.get(endpoints.specificDealershipById(dealershipId));
 
     const filteredAvailableOffers = dealershipEntity.availableOffers.filter((id) => id !== offerId);
     dealershipEntity.availableOffers = filteredAvailableOffers;
 
-    const result = await requestHTTP.put(endpoints.specificDealership(dealershipId), dealershipEntity);
+    const result = await requestHTTP.put(endpoints.specificDealershipById(dealershipId), dealershipEntity);
     return result;
 }
 
