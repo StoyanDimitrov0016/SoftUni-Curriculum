86ec3a545053c72c087b1cfa38e46b48cbe7b28b
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-12-09 00:55:28 +0200

    chore: Remove unused imports in index.jsx

--- 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/main.jsx
+++ 4-JS-WEB/2-Front-end-Framework/car-marketplace/client/src/main.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "./index.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./contexts/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
